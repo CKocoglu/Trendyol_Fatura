7ddf461a5aa731dd3fb10741d46c56213b93a211
CKocoglu <35204048+CKocoglu@users.noreply.github.com>
2021-04-04 16:08:31 +0300

    tc no kısmı eklendi, farklı ürün başlıkları ve adetleri fixlendi.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import date
 import csv
 today = date.today()
 file_name = "Tarih_" + str(today.strftime("%d-%m-%Y"))
-fieldnames = ["Siparis_no", "isim", "Farkli_urun_adedi", "Siparis_adedi"
+fieldnames = ["Siparis_no", "isim", "tc_no", "Siparis_adedi"
             , "Urun_adi", "Satis_tutari", "Kargo_kodu", "Teslimat_adı"
             , "Teslimat_adres", "Fatura_adı", "Fatura_adres"]
 
@@ -19,13 +19,13 @@ def initialize_csv_file():
         writer.writeheader()  # başlarına fieldnameleri ekliyor
         file.close()
 
-def append_csv_file(siparis_no,isim,farkli_urun_adedi,siparis_adet,urun_adi,satis_tutari
+def append_csv_file(siparis_no,isim,tc_no,siparis_adet,urun_adi,satis_tutari
                     ,kargo_kodu,teslimat_isim,teslimat_adres,fatura_isim,fatura_adres):
 
     with open (f"{file_name}.csv" , "a" , newline='')as file:
         writer = csv.DictWriter(file,fieldnames=fieldnames)
 
-        writer.writerow({"Siparis_no": siparis_no, "isim": isim, "Farkli_urun_adedi": farkli_urun_adedi,
+        writer.writerow({"Siparis_no": siparis_no, "isim": isim, "tc_no": tc_no,
                          "Siparis_adedi": siparis_adet , "Urun_adi": urun_adi, "Satis_tutari": satis_tutari,
                          "Kargo_kodu": kargo_kodu, "Teslimat_adı": teslimat_isim , "Teslimat_adres": teslimat_adres,
                          "Fatura_adı": fatura_isim, "Fatura_adres": fatura_adres})
@@ -82,80 +82,111 @@ if is_login == "go" :
     except:
         logging.error("Bir şeyler ters gitti.")
 
-time.sleep(2)
+time.sleep(3)
 #siparis sayisi 20 den büyükse gösterim sayısını 50 ye cıkar sayfada.
 siparis_sayisi = driver.find_element_by_xpath('//*[@id="shipment-packages"]/div/nav/div/ul/li[2]/a/p').text
 print("Toplam aktif sipariş sayınız: %s"%(siparis_sayisi))
 
+siparis_temp=""
 for key in str(siparis_sayisi):
     if key.isdigit():
-        siparis_sayisi=int(key)
+        siparis_temp=siparis_temp+key
+siparis_sayisi = int(siparis_temp)
+
+if siparis_sayisi > 20:
+    azami_siparis_xpath ='//*[@id="shipment-packages"]/div/div/div[1]/div[1]/nav/div/div/select'
+    azami_50_xpath = '//*[@id="shipment-packages"]/div/div/div[1]/div[1]/nav/div/div/select/option[3]'
+    WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,azami_siparis_xpath)))
+    driver.find_element_by_xpath(azami_siparis_xpath).click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, azami_50_xpath)))
+    driver.find_element_by_xpath(azami_50_xpath).click()
+
+
 
 print(siparis_sayisi)
 
 initialize_csv_file()
 print("Sipariş bilgileri alınıyor...")
-
-
-for i in range(2,siparis_sayisi+2):
-    print("%d.siparişin bilgileri alınıyor..."%(i-1))
-
-    siparis_no  = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[2]/div[1]/span').text
-    print("siparisno ",siparis_no)
-    isim        = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[4]').text
-    print("isim  ",isim)
-    #sonradan düzenle
-    farkli_urun_adedi="bura sonradan eklenecek."
-    siparis_adet=driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[5]').text
-    print("siparis adet ",siparis_adet)
-    urun_adi    =driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[6]/div/div[2]/a').text
-    print("urun adi  ",urun_adi)
-
+time.sleep(1)
+for i in range(2, siparis_sayisi + 2):
+    tbodys = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]')
+    trdata = tbodys.find_elements_by_tag_name("tr")
+    siparis_no = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[2]/div[1]/span').text
+    print("siparisno ", siparis_no)
+    isim = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[4]').text
+    print("isim  ", isim)
+    # sonradan düzenle
+    tc_no = 11111111111
+    siparis_adet = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[5]').text
+    print("siparis adet ", siparis_adet)
     try:
-        satis_tutari = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr[1]/td[9]/ul/li[3]/b').text
+        satis_tutari = driver.find_element_by_xpath(
+            f'//*[@id="mp-data-table"]/tbody[{i}]/tr[1]/td[9]/ul/li[3]/b').text
         satis_tutari = convert_stringToFloat(satis_tutari)
     except:
         satis_tutari = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/ul/li/b').text
         satis_tutari = convert_stringToFloat(satis_tutari)
         pass
-
     print("satis tutari ", satis_tutari)
-    kargo_kodu  =driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[8]/div[2]/span[2]').text
-    print("kargo kodu = ",kargo_kodu)
+    kargo_kodu = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[8]/div[2]/span[2]').text
+    print("kargo kodu = ", kargo_kodu)
 
-    #fatura islemleri kısmı
+    urun_adi = ""
+    print("len trdata = ", len(trdata))
+    if len(trdata) > 2:
+        for k in range(1, len(trdata)):
+            name = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr[{k}]')
+            title = name.find_element_by_class_name("product-name")
+            print(f"{i}.{k}.Title = {title.text}")
+            if k != 1:
+                urun_adet1 = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr[{k}]/td[1]').text
+                urun_adi = urun_adi + "@" +urun_adet1+title.text;
+            else:
+                urun_adet1 = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr[{k}]/td[5]').text
+                urun_adi = urun_adet1+title.text;
+        print(urun_adi.split("@"))
+    else:
+        urun_adi = driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[6]/div/div[2]/a').text
+        print("urun adi = ", urun_adi)
+
     try:
-        driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/span').click()
-    except:
-        driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/div/div[1]/div[1]').click()
+        fatura_islemleri_xpath = f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/div/div[1]/div/span'
+        fatura_bilgileri_xpath = f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/div/div[1]/div[2]/div/button[2]'
         time.sleep(1)
-        driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/div/div[1]/div[2]/div/button[2]').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, fatura_islemleri_xpath)))
+        driver.find_element_by_xpath(fatura_islemleri_xpath).click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, fatura_bilgileri_xpath)))
+        driver.find_element_by_xpath(fatura_bilgileri_xpath).click()
+        time.sleep(1)
+
+    except:
+        driver.find_element_by_xpath(f'//*[@id="mp-data-table"]/tbody[{i}]/tr/td[9]/span').click()
         pass
 
     # time.sleep(2)
     print("fatura işlemleri kısmına girdim.")
-    title = driver.find_element_by_xpath('//*[@id="shipment-packages"]/div/div/div[4]/div/div/div/form/div[1]/h5').text
-    print("title" ,title)
+    # title = driver.find_element_by_xpath('//*[@id="shipment-packages"]/div/div/div[4]/div/div/div/form/div[1]/h5').text
+    # print("title" ,title)
 
-    teslimat_isim  = driver.find_element_by_xpath('//*[@id="printable-content"]/p[1]').text
+    teslimat_isim = driver.find_element_by_xpath('//*[@id="printable-content"]/p[1]').text
     teslimat_isim = fix_name(teslimat_isim)
-    print("teslimat isim ",teslimat_isim)
+    print("teslimat isim ", teslimat_isim)
 
     teslimat_adres = driver.find_element_by_xpath('//*[@id="printable-content"]/p[2]').text
     teslimat_adres = fix_adress(teslimat_adres)
-    print("teslimat adres ",teslimat_adres)
-    fatura_isim    = driver.find_element_by_xpath('//*[@id="printable-content"]/p[3]').text
+    print("teslimat adres ", teslimat_adres)
+    fatura_isim = driver.find_element_by_xpath('//*[@id="printable-content"]/p[3]').text
     fatura_isim = fix_name(fatura_isim)
-    print("fatura isim ",fatura_isim)
-    fatura_adres   = driver.find_element_by_xpath('//*[@id="printable-content"]/p[4]').text
+    print("fatura isim ", fatura_isim)
+    fatura_adres = driver.find_element_by_xpath('//*[@id="printable-content"]/p[4]').text
     fatura_adres = fix_adress(fatura_adres)
-    print("fatura adres ",fatura_adres)
+    print("fatura adres ", fatura_adres)
 
     driver.find_element_by_xpath('//*[@id="shipment-packages"]/div/div/div[4]/div/div/div/form/div[1]/button/span').click()
 
-    print("%d.sipariş bilgileri alındı.Dosyaya ekleniyor..."%(i-1))
+    print("%d.sipariş bilgileri alındı.Dosyaya ekleniyor..." % (i - 1))
     # ekleme
-    append_csv_file(siparis_no, isim, farkli_urun_adedi, siparis_adet, urun_adi, satis_tutari
+    append_csv_file(siparis_no, isim, tc_no, siparis_adet, urun_adi, satis_tutari
                     , kargo_kodu, teslimat_isim, teslimat_adres, fatura_isim, fatura_adres)
 
     print("%d.siparis bilgisi dosyaya eklendi.\n \n"%(i-1))
@@ -163,4 +194,3 @@ for i in range(2,siparis_sayisi+2):
 print("Program başarıyla sonlandı. %d sipariş kaydedildi."%siparis_sayisi)
 print("Program kapatılıyor.")
 driver.quit()
-
